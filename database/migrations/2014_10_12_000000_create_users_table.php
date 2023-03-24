d0e8cb0ea9130d845c12f2426cc4b301a77ad80b
64160269 <118897634+64160269@users.noreply.github.com>
2023-03-25 03:43:17 +0700

    get

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fname');
+            $table->string('lname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('gender');
+            $table->string('picture');
+            $table->unsignedbiginteger('dep_id');
             $table->rememberToken();
             $table->timestamps();
         });
